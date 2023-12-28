b8c088105d126b765d226bf27299fe03f2cb2ee9
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-28 14:48:03 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yuga Samuel on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
