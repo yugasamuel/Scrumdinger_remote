5638c778903cb63072d45cff5879314adcdaeac8
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-28 15:11:49 +0700

    Create card view

---
+++ Scrumdinger/LabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  LabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Yuga Samuel on 28/12/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
