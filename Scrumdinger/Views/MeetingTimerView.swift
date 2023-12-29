ab98ab46665b74705875833b2a147ba727448311
Yuga Samuel <yugasamuel2002@gmail.com>
2023-12-29 11:21:23 +0700

    Create meeting timer view

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,42 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Yuga Samuel on 29/12/23.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    let theme: Theme
+    
+    private var currentSpeaker: String {
+        speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
+    }
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay {
+                VStack {
+                    Text(currentSpeaker)
+                        .font(.title)
+                    Text("is speaking")
+                }
+                .accessibilityElement(children: .combine)
+                .foregroundStyle(theme.accentColor)
+            }
+    }
+}
+
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var speakers: [ScrumTimer.Speaker] {
+        [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
+    }
+    
+    static var previews: some View {
+        MeetingTimerView(speakers: speakers, theme: .yellow)
+    }
+}
